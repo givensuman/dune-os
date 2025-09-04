4e9a1f2ae711aa79eac19f40b4f5afe9cd2b6d6e
givensuman <givensuman@duck.com>
2025-09-04 12:29:05 -0400

    Add back in container packages

--- scripts/00-packages.sh
+++ scripts/00-packages.sh
@@ -3,14 +3,33 @@
 set -ouex pipefail
 
 mkdir -p /etc/yum.repos.d
+sudo dnf5 -y install dnf-plugins-core
 
 curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo | tee /etc/yum.repos.d/terra.repo
 dnf5 -y install terra-release
+
+sudo dnf5 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
 packages=(
   ghostty
   git
   vlc
+  p7zip
+  p7zip-plugins
+  docker-buildx-plugin
+  docker-ce
+  docker-ce-cli
+  docker-compose-plugin
+  containerd.io
+  podman-bootc
+  podman-compose
+  podman-machine
+  podman-tui
+  podmansh
 )
 
 dnf5 -y install "${packages[@]}"
+
+if rpm -q docker-ce >/dev/null; then
+  systemctl enable docker.socket
+fi
