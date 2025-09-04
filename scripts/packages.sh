8a0ba2b93065c552e0bff6c19c9fa9649a4acd5b
givensuman <givensuman@duck.com>
2025-09-04 09:51:05 -0400

    Remove Docker support

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -7,19 +7,10 @@ mkdir -p /etc/yum.repos.d
 curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo | tee /etc/yum.repos.d/terra.repo
 dnf5 -y install terra-release
 
-curl -fsSL https://download.docker.com/linux/fedora/docker-ce.repo | tee /etc/yum.repos.d/docker-ce.repo
-
 packages=(
   ghostty
   git
   vlc
-  docker-ce
-  docker-ce-cli
-  containerd.io
-  docker-buildx-plugin
-  docker-compose-plugin
 )
 
 dnf5 -y install "${packages[@]}"
-
-systemctl enable docker
