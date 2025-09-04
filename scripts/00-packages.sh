58d2cdfbc2c276da93215d9d840d39b984077ae8
given <givensuman@gmail.com>
2025-09-04 13:00:18 -0400

    Add `dnf-plugins-core`

--- scripts/00-packages.sh
+++ scripts/00-packages.sh
@@ -7,6 +7,7 @@ mkdir -p /etc/yum.repos.d
 curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo | tee /etc/yum.repos.d/terra.repo
 dnf5 -y install terra-release
 
+dnf5 -y install dnf-plugins-core
 dnf5 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
 packages=(
