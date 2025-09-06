b87bb9d37be86b0f50ddc888231811f49e7328eb
givensuman <givensuman@duck.com>
2025-09-06 14:31:22 -0400

    Remove redundant stuff

--- scripts/02-cosmic.sh
+++ scripts/02-cosmic.sh
@@ -2,11 +2,10 @@
 
 set -ouex pipefail
 
-dnf5 -y swap @gnome-desktop @cosmic-desktop
-dnf5 -y remove @gnome-desktop gnome # Redundant
-dnf5 -y install @cosmic-desktop     # Redundant
+systemctl disable gdm.service
 
-systemctl disable gdm.service # Redundant
+dnf5 -y swap @gnome-desktop @cosmic-desktop
+
 systemctl enable cosmic-greeter.service
 
 fc-cache -f /usr/share/fonts/Hack
