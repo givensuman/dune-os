700d08159e18ef83f71def5107e89947db7b08c8
givensuman <givensuman@duck.com>
2025-09-09 10:40:31 -0400

    Move some files around

--- scripts/01-flatpaks.sh
+++ scripts/01-flatpaks.sh
@@ -2,20 +2,27 @@
 
 set -ouex pipefail
 
-flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# Remove Fedora remote and prefer Flathub
+# Fedora remote seems to break `cosmic-store`
 systemctl disable flatpak-add-fedora-repos.service
+flatpak remote-delete fedora
+flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Prefer Flatpak version of Firefox
 dnf5 -y remove firefox firefox-langpacks
 rm -rf /usr/share/mozilla
 
 flatpaks=(
+  # Useful for atomic systems
   app/com.github.tchx84.Flatseal
   app/io.github.flattool.Warehouse
   app/io.github.flattool.Ignition
   app/io.github.flattool.Warehouse
   app/io.gitlab.adhami3310.Impression
   app/io.missioncenter.MissionCenter
+  app/io.podman_desktop.PodmanDesktop
 
+  # GNOME goodies
   app/org.gnome.Calculator
   app/org.gnome.Calendar
   app/org.gnome.Characters
@@ -28,12 +35,14 @@ flatpaks=(
   app/org.gnome.Maps
   app/org.gnome.Papers
   app/org.gnome.SimpleScan
+  app/run org.gnome.Snapshot
   app/org.gnome.Weather
   app/org.gnome.clocks
   app/org.gnome.font-viewer
 
-  app/io.podman_desktop.PodmanDesktop
+  # Opinionated additions
   app/org.mozilla.firefox
+  app/io.github.nozwock.Packet
 )
 
 mkdir -p /var/roothome
--- scripts/03-ublue.sh
+++ scripts/03-ublue.sh
@@ -10,6 +10,7 @@ dnf5 -y install ublue-os-media-automount-udev
 
 systemctl --global enable podman.socket
 
+# Stellarite fixes
 systemctl enable brew-dir-fix.service
 systemctl enable brew-setup.service
 systemctl disable brew-upgrade.timer
@@ -27,10 +28,10 @@ else
 fi
 
 # Disable COPRs and RPM Fusion Repos
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 dnf5 -y copr disable ublue-os/staging
 dnf5 -y copr disable ublue-os/packages
 dnf5 -y copr disable phracek/PyCharm
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 for i in /etc/yum.repos.d/rpmfusion-*; do
