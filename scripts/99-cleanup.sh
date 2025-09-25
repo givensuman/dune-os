c011f073dbc35905cb48f0fe1ca6f85d95ec433e
givensuman <givensuman@duck.com>
2025-09-25 17:02:49 -0400

    Move some functions into Justfile form

--- scripts/99-cleanup.sh
+++ scripts/99-cleanup.sh
@@ -2,10 +2,9 @@
 
 set -ouex pipefail
 
-shopt -s extglob
 dnf5 clean all
 
 rm -rf /tmp/* || true
-rm -rf /var/!(cache)
-rm -rf /var/cache/!(rpm-ostree)
+rm -rf /var/log/*
+rm -rf /var/lib/dnf5/*
 rm -rf /scripts
