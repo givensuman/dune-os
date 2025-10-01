79237fcfc37e8e05b5dcea1e35fbe1d953dd2ddb
givensuman <givensuman@duck.com>
2025-10-01 12:46:42 -0400

    Revert Bazzite

--- scripts/03-cosmic.sh
+++ scripts/03-cosmic.sh
@@ -4,7 +4,16 @@ set -ouex pipefail
 
 dnf5 -y swap @gnome-desktop @cosmic-desktop
 
-systemctl enable cosmic-greeter.service
+if systemctl is-active --quiet gdm; then
+  systemctl disable gdm
+fi
+if systemctl is-active --quiet lightdm; then
+  systemctl disable lightdm
+fi
+if systemctl is-active --quiet sddm; then
+  systemctl disable sddm
+fi
+systemctl enable cosmic-greeter
 
 fc-cache -f /usr/share/fonts/Hack
 fc-cache -f /usr/share/fonts/Inter
